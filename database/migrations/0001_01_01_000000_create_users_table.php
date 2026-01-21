82b59f735c801a10ed227998b3170e67fdbe401e
Esther Damaris <esther.damaris.rojas.123@gmail.com>
2026-01-21 12:42:32 -0500

    Creacion de la db y semillas agregadas(falta de eventos,participantes y certificados)

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-             $table->string('dni',8);
+            $table->string('dni',8);
             $table->rememberToken();
         });
 
